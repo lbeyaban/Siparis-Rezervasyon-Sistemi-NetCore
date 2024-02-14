bbf7ce82528c7f3da749a958ff7ca22484fcc098
Ömer Aydin <63254747+lbeyaban@users.noreply.github.com>
2024-02-14 20:53:52 +0300

    HomePage Design

---
+++ SignalRWebUI/Dtos/SliderDto/ResultSliderDto.cs
@@ -0,0 +1,9 @@
+﻿namespace SignalRWebUI.Dtos.SliderDto
+{
+    public class ResultSliderDto
+    {
+        public int SliderID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
