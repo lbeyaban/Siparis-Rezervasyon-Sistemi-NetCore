e67146b292455f0f1c93cbfd71ca57fe8cdb1545
omeraydin <210202109@kocaeli.edu.tr>
2024-02-16 16:13:02 +0300

    Add BasketManager
    Basket manager added.

--- SignalR.BusinessLayer/Concrete/BasketManager.cs
+++ SignalR.BusinessLayer/Concrete/BasketManager.cs
@@ -5,13 +5,39 @@ using SignalR.EntityLayer.Entities;
 
 namespace SignalR.BusinessLayer.Concrete
 {
-	public class BasketManager
-    { 
+    public class BasketManager : IBasketService
+    {
+        private readonly IBasketDal _basketDal;
 
+        public BasketManager(IBasketDal basketDal)
+        {
+            _basketDal = basketDal;
+        }
 
+        public void TAdd(Basket entity)
+        {
+            _basketDal.Add(entity);
+        }
 
+        public void TDelete(Basket entity)
+        {
+            _basketDal.Delete(entity);
+        }
 
-       
+        public List<Basket> TGetAll()
+        {
+            return _basketDal.GetAll();
+        }
+
+        public Basket TGetById(int id)
+        {
+            return _basketDal.GetById(id);
+        }
+
+        public void TUpdate(Basket entity)
+        {
+            _basketDal.Update(entity);
+        }
     }
 }
 
